fbfd79e64f0b238fe9a6d0ab90493988c805a1d8
JoeChang3816 <99543725+JoeChang3816@users.noreply.github.com>
2023-04-14 00:35:01 +0800

    Update navBar.php

--- WA_Group/navBar.php
+++ WA_Group/navBar.php
@@ -1,3 +1,5 @@
+<?php $username = $_SESSION['username']; ?>
+
 <nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
 <div class="container-lg">
     <!-- Brand -->
@@ -28,7 +30,12 @@
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="account.php">Account</a></li>
                     <li><a class="dropdown-item" href="cart.php">Cart</a></li>
+                    
+                    //Only Admin will have this option
+                    <?php if($username == "Admin"){ ?>
                     <li><a class="dropdown-item" href="admin_Product.php">Manage Products</a></li>
+                    <?php}?>
+                    
                     <li>
                         <hr class="dropdown-divider">
                     </li>
