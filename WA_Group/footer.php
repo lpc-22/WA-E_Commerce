11934a025d3bee5b1ca884a1b475c923de0997d8
JoeChang3816 <99543725+JoeChang3816@users.noreply.github.com>
2023-04-13 21:05:55 +0800

    Add files via upload

---
+++ WA_Group/footer.php
@@ -0,0 +1,33 @@
+<?php 
+
+echo '
+    <!-- Site footer -->
+    <footer class="site-footer">
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Welcome to Petify, your one-stop online shop for all your pet needs! We are a team of passionate animal lovers who believe that pets are an important part of our lives and deserve the best care and attention.</p>
+            </div>
+            <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+            </div>
+        </div>
+        <hr>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by COMP3421
+            </p>
+            </div>
+        </div>
+        </div>
+    </footer>
+    ';
+
+?>
