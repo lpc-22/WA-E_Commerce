9c56f6498035d6e331ec878d7478cbdbf4afff21
yeunghoiho <18052583d@connect.polyu.hk>
2023-04-13 16:36:45 +0800

    Update login part

--- WA_Group/login.php
+++ WA_Group/login.php
@@ -1,6 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+session_start();
 
 require_once 'connection.php';
 
@@ -8,18 +7,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT name, password FROM users WHERE name = ?";
+    $sql = "SELECT name, email, password FROM users WHERE name = ?";
 
     if ($stmt = $link->prepare($sql)) {
         $stmt->bind_param("s", $username);
         $stmt->execute();
-
-        $stmt->bind_result($dbUsername, $dbPassword);
+        $stmt->bind_result($dbUsername, $dbEmail, $dbPassword);
 
         if ($stmt->fetch()) {
             if (strcmp($password, $dbPassword) === 0) {
-                // Redirect to account.html
-                header("Location: account.html");
+                // Store user data in session and redirect to account.php
+                $_SESSION['username'] = $dbUsername;
+                $_SESSION['email'] = $dbEmail;
+                header("Location: account.php");
                 exit;
             } else {
                 echo "Incorrect password.";
