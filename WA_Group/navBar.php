9202b4f46121bc14895380637787f5bcfcb5ab9d
darrennn7 <68835909+darrennn7@users.noreply.github.com>
2023-04-14 00:52:26 +0800

    Update navBar.php

--- WA_Group/navBar.php
+++ WA_Group/navBar.php
@@ -25,8 +25,7 @@
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="account.php">Account</a></li>
                     <li><a class="dropdown-item" href="cart.php">Cart</a></li>
-                    
-                   
+                    <li><a class="dropdown-item" href="vieworder.php">View My Order</a></li>
                     <li><a class="dropdown-item" href="admin_Product.php">Manage Products</a></li>
                     
                     <li>
