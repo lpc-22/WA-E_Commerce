f6912f7f8c55567f9b44db27a2d36a4021feb704
JoeChang3816 <99543725+JoeChang3816@users.noreply.github.com>
2023-04-13 22:02:31 +0800

    Update product.js

--- WA_Group/Script/product.js
+++ WA_Group/Script/product.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-    var Product_List = document.getElementById("product_list");
+
+    //*******************FILTERING*******************//
+
     var Filter_All = document.getElementById("ProductFilter_All");
     var Filter_Food = document.getElementById("ProductFilter_Food");
     var Filter_Toy = document.getElementById("ProductFilter_Toy");
@@ -50,4 +52,29 @@ window.onload = function() {
 
 
     });
-};
+
+    //*******************SEARCHING*******************//
+
+
+
+};    
+
+function SearchingProduct(){
+
+    var query = document.getElementById('productSearchingBar').value.toUpperCase();
+    var productList = document.getElementsByClassName('products');
+
+    //Loop through all products
+    for (var i = 0; i < productList.length; i++) {
+
+        var currentProductName = productList[i].getElementsByTagName("h5")[0].innerText;
+
+        //Hide those don't match the search query
+        if (currentProductName.toUpperCase().indexOf(query) == -1) {
+            productList[i].style.display = "none";
+        }else{
+            productList[i].style.display = "";
+        }
+    }
+
+}
