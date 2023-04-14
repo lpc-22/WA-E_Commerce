a498153a0f3718e157e08a5e01ca475948498422
darrennn7 <68835909+darrennn7@users.noreply.github.com>
2023-04-14 16:08:28 +0800

    update

--- WA_Group/about.php
+++ WA_Group/about.php
@@ -46,6 +46,7 @@
         </div>
     </div>
 
+    <?php include("footer.php") ?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
--- WA_Group/contact.php
+++ WA_Group/contact.php
@@ -48,7 +48,6 @@
         </div>
     <!-- </div> -->
 </div>
-<?php include("footer.php") ?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
