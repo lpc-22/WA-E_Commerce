dbfdaaab9c5d45caf1f93b4a922e05ad38733b14
yeunghoiho <18052583d@connect.polyu.hk>
2023-04-14 10:29:45 +0800

    update signup part

--- WA_Group/signup.php
+++ WA_Group/signup.php
@@ -1,5 +1,10 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="light">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +13,9 @@
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="Style/style.css">
-    <title>Login - Online Pet Store</title>
+    <title>Sign Up - Online Pet Store</title>
     <style>
-        .login-container {
+        .signup-container {
             max-width: 400px;
             margin: 5% auto;
             padding: 20px;
@@ -31,24 +36,33 @@
 
     </style>
 </head>
+
 <body>
     <?php include("navBar.php")?>
 	
     <div class="container">
-        <div class="login-container">
-            <h2 class="text-center mb-4">Online Pet Store</h2>
-            <form action="loginSession.php" method="post">
+        <div class="signup-container">
+            <h2 class="text-center mb-4">Sign Up</h2>
+            <form action="signupSession.php" method="post">
                 <div class="input-group mb-3">
                     <span class="input-group-text"><i class="fas fa-user"></i></span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" name="username">
+                    <input type="text" class="form-control" placeholder="Name" aria-label="Name" name="name" required>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                    <input type="email" class="form-control" placeholder="Email" aria-label="Email" name="email" required>
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                    <input type="password" class="form-control" placeholder="Password" aria-label="Password" name="password">
+                    <input type="password" class="form-control" placeholder="Password" aria-label="Password" name="password" required>
                 </div>
-                <button type="submit" class="btn btn-primary w-100 mb-3">Sign In</button>
+                <div class="input-group mb-3">
+                    <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                    <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" name="confirmPassword" required>
+                </div>
+                <button type="submit" class="btn btn-primary w-100 mb-3">Create Account</button>
             </form>
-            <p class="text-center">Don't have an account? <a href="signup.php">Sign Up</a></p>
+            <p class="text-center">Already have an account? <a href="login.php">Sign In</a></p>
         </div>
     </div>
 
@@ -56,4 +70,5 @@
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
 </body>
+
 </html>
---
+++ WA_Group/signupSession.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+require_once 'connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    if ($password !== $confirmPassword) {
+        $_SESSION['signup_error'] = "Passwords do not match";
+        header("Location: signup.php");
+        exit;
+    }
+
+    $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+    if ($stmt = $link->prepare($sql)) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt->bind_param("sss", $name, $email, $hashedPassword);
+
+        if ($stmt->execute()) {
+            $_SESSION['signup_success'] = true;
+			?>
+			<script>
+				alert("Create account successfully");
+				window.location.href = 'login.php';
+			</script>
+			<?php
+        } else {
+            $_SESSION['signup_error'] = "Error: Unable to create account. " . $link->error;
+            header("Location: signup.php");
+            exit;
+        }
+
+        $stmt->close();
+    } else {
+        $_SESSION['signup_error'] = "Error: Unable to prepare statement. " . $link->error;
+        header("Location: signup.php");
+        exit;
+    }
+
+    $link->close();
+} else {
+    header("Location: signup.php");
+    exit;
+}
+?>
