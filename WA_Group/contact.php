e26ed680908c735187c6afbfd1f72e780eba0b04
isaaclee <isaaclpc0622@gmail.com>
2023-04-14 13:50:01 +0800

    update Contact page

--- WA_Group/contact.php
+++ WA_Group/contact.php
@@ -12,12 +12,42 @@
 </head>
 
 <body>
-<?php 
-include("navBar.php") ?>
+<?php include("navBar.php") ?>
 
+<div class="container-lg">
+    <div class="grid">
+        <div class="g-col-6">
+            <h1>Contact us</h1>
+            <p>Esse voluptate proident officia cillum amet. Incididunt ipsum qui et velit ex exercitation aliqua adipisicing culpa nulla consectetur Lorem exercitation sint incididunt. Proident adipisicing nulla officia non consectetur. Aliqua veniam ad nulla deserunt aute elit ad occaecat esse eiusmod. Id ullamco ea aliqua amet amet amet ea culpa voluptate aliqua. Nulla veniam veniam mollit deserunt laboris et pariatur ullamco officia ex dolore excepteur. Nostrud id cupidatat esse consequat in occaecat excepteur velit elit sint quis. Consectetur qui enim laboris do sint fugiat esse eiusmod aute ullamco ea reprehenderit adipisicing.</p>
+            <h1>Address:</h1>
+            <p>Room xxx, Block xxx, X/F XXXX XXXX XX</p>
+            <p>xx xxxxx xx Street, xxx xxx xxx, Hong Kong</p>
+            <h1>Other:</h1>
+            <p>Whatsapp/Call: +852 xxxx xxxx</p>
+            <p>Email: petify1234@gmail.com</p>
+            <h1>Open Hours:</h1>
+            <p>Mon-Fri: 10:00 - 20:00</p>
+            <p>Sat-Sun: 11:00 - 18:00</p>
+        </div>
+        <div class="g-col-6">
+            <form action="">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Name</label>
+                <input class="form-control" id="exampleFormControlInput1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
 
-
-
+            </form>
+        </div>
+    <!-- </div> -->
+</div>
 
 </body>
 
