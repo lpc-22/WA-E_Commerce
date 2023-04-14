81464380cc2e59a35467d9888256e8c2139bd2f3
JoeChang3816 <99543725+JoeChang3816@users.noreply.github.com>
2023-04-14 19:34:08 +0800

    Update comp3421.sql

--- WA_Group/comp3421.sql
+++ WA_Group/comp3421.sql
@@ -197,3 +197,63 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1:3307
+-- 產生時間： 2023 年 04 月 14 日 13:31
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `comp3421`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(3, 'John', 'John@gmail.com', '$2y$10$5WzvFC1Z5kAPkbjhzJtYN.2OeNCzAwqzn9yC7PN.yhb4RfniZJbfq'),
+(4, 'Tim', 'Tim@gmail.com', '$2y$10$O84ghSErSPoG2u8L/jI9uO/pj0UzqkAyE10fpMRuoWb95K2m5KP1u'),
+(5, 'Admin', 'admin@gmail.com', '$2y$10$pNA2TjV8Zzx2N992peRcsOEgPQ8KRFUkDYxPnDUoQqQ4LOlEG8ILq');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `users`
+--
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
